8de131ab97cd41dcfff59768ec9654528fb863a1
Jhon Sidney <jhon.sidney@gmail.com>
2021-03-19 16:27:32 -0300

    changes made next step will be step 9

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,10 +1,10 @@
 class ArticlesController < ApplicationController
   def index
-	  @articles = Article.all
+    @articles = Article.all
   end
 
   def show
-	@article =  Article.find(params[:id]) 
+    @article = Article.find(params[:id])
   end
 
   def new
@@ -21,9 +21,29 @@ class ArticlesController < ApplicationController
     end
   end
 
-  private
-  def article_params
-    params.require(:article).permit(:title, :body)
+  def edit
+    @article = Article.find(params[:id])
   end
 
+  def update
+    @article = Article.find(params[:id])
+
+    if @article.update(article_params)
+      redirect_to @article
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @article = Article.find(params[:id])
+    @article.destroy
+
+    redirect_to root_path
+  end
+
+  private
+  def article_params
+    params.require(:article).permit(:title, :body, :status)
+  end
 end
